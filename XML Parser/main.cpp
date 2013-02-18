d5fd1a63ce8dfc4e33f3ac63a594b3e318bb3580
James Kizer <jk@Jamess-MacBook-Pro.local>
2013-02-18 13:22:53 -0500

    Initial Commit

---
+++ XML Parser/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  XML Parser
+//
+//  Created by James Kizer on 2/18/13.
+//  Copyright (c) 2013 James Kizer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
